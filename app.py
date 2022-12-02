1483abe3e888ea18a86c37efddef4ed57dafa3af
ajitchincholkar <chincholkarajit@gmail.com>
2022-12-02 16:00:44 +0530

    third commit

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def structural_sim(img1, img2):
 
 st.title('JPEG to NFT # Finder')
 
-nft_collections = ['Azuki', 'Moonbirds', 'Doodles', 'BAYC', 'WoW', 'CloneX', 'MAYC']
+nft_collections = ['Azuki', 'Moonbirds', 'Doodles', 'BAYC', 'WoW', 'CloneX', 'MAYC', 'Cool Cats', 'Pudgy Penguins']
 
 selected_collection = st.selectbox('Select Collection', nft_collections)
 
@@ -244,6 +244,70 @@ if selected_collection == 'MAYC':
             max_score = max(scores)
 
             for i in range(len(filenames_mayc)):
+                if name_scores[i][1] == max_score:
+                    final_nft = name_scores[i][0][:-4]
+                    st.subheader(final_nft)
+
+
+if selected_collection == 'Cool Cats':
+    filenames_cc = os.listdir('Cool Cats')
+
+    if uploaded_img is not None:
+
+        if save_uploaded_image(uploaded_img):
+            display_img = Image.open(uploaded_img)
+            st.image(display_img, width=200)
+
+            img1 = cv2.imread(os.path.join('Uploads', uploaded_img.name), 0)
+            resized_img = resize(img1, (128, 128), anti_aliasing=True, preserve_range=True)
+
+            name_scores = []
+            scores = []
+
+            for name in filenames_cc:
+                img = cv2.imread(os.path.join('Cool Cats', name), 0)
+                ssim = structural_sim(img, resized_img)
+
+                name_score = (name, ssim)
+                scores.append(ssim)
+
+                name_scores.append(name_score)
+
+            max_score = max(scores)
+
+            for i in range(len(filenames_cc)):
+                if name_scores[i][1] == max_score:
+                    final_nft = name_scores[i][0][:-4]
+                    st.subheader(final_nft)
+
+
+if selected_collection == 'Pudgy Penguins':
+    filenames_pudpen = os.listdir('Pudgy Penguins')
+
+    if uploaded_img is not None:
+
+        if save_uploaded_image(uploaded_img):
+            display_img = Image.open(uploaded_img)
+            st.image(display_img, width=200)
+
+            img1 = cv2.imread(os.path.join('Uploads', uploaded_img.name), 0)
+            resized_img = resize(img1, (128, 128), anti_aliasing=True, preserve_range=True)
+
+            name_scores = []
+            scores = []
+
+            for name in filenames_pudpen:
+                img = cv2.imread(os.path.join('Pudgy Penguins', name), 0)
+                ssim = structural_sim(img, resized_img)
+
+                name_score = (name, ssim)
+                scores.append(ssim)
+
+                name_scores.append(name_score)
+
+            max_score = max(scores)
+
+            for i in range(len(filenames_pudpen)):
                 if name_scores[i][1] == max_score:
                     final_nft = name_scores[i][0][:-4]
                     st.subheader(final_nft)
